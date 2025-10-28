e2cb3143635708d9cc324c1a02ff29c6152b1619
yossi lerer <ylerer770@gmail.com>
2025-10-28 18:17:00 +0200

    finish the building

--- game/game.py
+++ game/game.py
@@ -2,8 +2,7 @@ from copy import deepcopy
 
 from create.create import mix_cards
 
-def Inverted_table():
-    cards_to_print, num1, num2 = mix_cards()
+def Inverted_table(num1 ,num2):
 
     table = []
     for i in range(num1):
@@ -11,7 +10,7 @@ def Inverted_table():
         for j in range(num2):
             colu.append("X")
         table.append(colu)
-    return table, cards_to_print, num1, num2
+    return table
 
 def choice(num1,num2):
     choice1 = int(input(f"Choose a place in the line to turn - (0/{num1 -1}) : "))
@@ -19,30 +18,41 @@ def choice(num1,num2):
     return choice1, choice2
 
 
-def card_selection():
-    table, cards_to_print, num1, num2 = Inverted_table()
-    table_change = deepcopy(table)
+def card_selection(table, cards_to_print, num1, num2):
+
     print(table)
     print()
     print("- - - - - - -")
     print()
+    try:
+        n1, n2 = choice(num1,num2)
 
-    n1, n2 = choice(num1,num2)
+        card1 = cards_to_print[n1][n2]
+        table[n1][n2] = card1
+        print(table)
 
-    card1 = cards_to_print[n1][n2]
-    table[n1][n2] = card1
-    print(table)
+        n3,n4 = choice(num1,num2)
 
-    n3,n4 = choice(num1,num2)
+        card2 = cards_to_print[n3][n4]
+        table[n3][n4] = card2
+        print(table)
+        # return card1, card2, table, cards_to_print
 
-    card2 = cards_to_print[n3][n4]
-    table[n3][n4] = card2
-    print(table)
+        if card1 == card2:
+            return table
+        else:
+            table[n1][n2] = "X"
+            table[n3][n4] = "X"
+            return table
 
-    if card1 == card2:
-        return table
-    else:
-        return table_change
+    except:
+        print("You exceeded the number")
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from game.game import card_selection
+from game.game import card_selection, Inverted_table
+from create.create import mix_cards
 
-print(card_selection())
+cards_to_print, num1 ,num2 = mix_cards()
+table = Inverted_table(num1 ,num2)
+while True:
+    table = card_selection(table, cards_to_print, num1, num2)
+
+    x = True
+    for i in table:
+        if "X" in i:
+            x = False
+    if x == False:
+        continue
+    else:
+        print("you winn!!!")
+        print(table)
+        break
 
 
